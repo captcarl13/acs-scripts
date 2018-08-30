44036f5b63793aa8c14c3aab6a5d5fd6d8021716
capt_carl <captcarl13@gmail.com>
2018-08-30 19:57:02 -0400

    Added more format and initialize options.

--- winpe.ps1
+++ winpe.ps1
@@ -2,6 +2,11 @@
   Maybe add the ability to create Bootable Media with an existing WinPE environment?
   Add this to the existing system's hidden menu? -cv
 #>
+<#
+NOTE: This script is intended to work only in a preimage environment.
+If you want to create media in an existing Windows environment, follow the documentation
+located in \\owcaddist-srv\GhostImages\WinPE\WINDOWS PE IMAGE AUTOMATION.docx
+#>
 wait 1
 Write-Output NYIT ACS WinPE bootable media creation script, version PowerShell
 wait 1
@@ -9,14 +14,10 @@ wait 1
 DISKPART BEGINS HERE
 #>
 Get-Disk | Where-Object -FilterScript {$_.Bustype -Eq "USB"}
-#READ (lines 15-25) goes here // called CHOICE on Windows
-select disk $input
-Clear-Disk
-clean
-create partition primary
-select partition 1
-active
-format -q fs=FAT32
+wait 3
+Clear-Disk -Number 1 -RemoveData
+wait 3
+Initialize-Disk -Number 1 -PartitionStyle MBR
 <#
   Force assign to always mount USB to a unique drive letter, like U:\?
   By doing this, the script could run fully-automated?
