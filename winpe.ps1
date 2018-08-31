fc5a5a1b4a933051b283c929a37b7f0f2d58be84
capt_carl <captcarl13@gmail.com>
2018-08-31 14:50:53 -0400

    Lots of stuff.

--- winpe.ps1
+++ winpe.ps1
@@ -3,29 +3,25 @@
   Add this to the existing system's hidden menu? -cv
 #>
 <#
-NOTE: This script is intended to work only in a preimage environment.
-If you want to create media in an existing Windows environment, follow the documentation
-located in \\owcaddist-srv\GhostImages\WinPE\WINDOWS PE IMAGE AUTOMATION.docx
+  NOTE: This script is intended to work only in a preimage environment.
+  If you want to create media in an existing Windows environment, follow the documentation
+  located in \\owcaddist-srv\GhostImages\WinPE\WINDOWS PE IMAGE AUTOMATION.docx
 #>
-wait 1
+timeout 1
 Write-Output "NYIT ACS WinPE bootable media creation script, version PowerShell"
-wait 1
+timeout 1
 <#
-DISKPART BEGINS HERE
+  DISKPART BEGINS HERE
 #>
-Get-Disk 1 | Clear-Disk -RemoveData
+Get-Disk 1 | Clear-Disk -RemoveData #prompts user to confirm, possibly always confirm as yes?
 New-Partition -DiskNumber 1 -UseMaximumSize -IsActive -DriveLetter U | Format-Volume -FileSystem FAT32 -NewFileSystemLabel ACS
 <#
   Force assign to always mount USB to a unique drive letter, like U:\?
   By doing this, the script could run fully-automated?
+  DISKPART ENDS HERE
+  ROBOCOPY BEGINS HERE
 #>
-assign
-exit
-<#
-DISKPART ENDS HERE
-#ROBOCOPY BEGINS HERE
-#>
-ROBOCOPY /S /XO O:\WinPE\USB\ U:\ *.* # whatever USB drive letter may be needs to go here
+ROBOCOPY /S /XO O:\WinPE\USB\ U:\
 <#
   ROBOCOPY ENDS HERE
   end of script
